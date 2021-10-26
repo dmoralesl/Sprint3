37ea17b891f858de1e895fb913133bd767c37247
David Morales <5dmorales474@gmail.com>
2021-10-26 18:48:25 +0200

    Level 1

--- js/checkout.js
+++ js/checkout.js
@@ -1,14 +1,56 @@
-// Get the input fields
-var password = document.querySelector(".password");
-var phone = document.querySelector('.phone');
-var name = document.querySelector('.name');
-
 // Get the error elements
-var errorPassword = document.getElementById("errorPassword");
-var errorName = document.getElementById('errorName');  
-var errorPhone = document.getElementById('errorPhone');  
+let errorPassword = document.getElementById("errorPassword");
+let errorPhone = document.getElementById('errorPhone');  
+let errorEmail = document.getElementById('errorEmail');  
+
+const form = document.querySelector('.form');
 
 // Exercise 9
-function validate() {
+function validate(event) {
+    console.log(event)
+    event.preventDefault();
+        
     // Validate fields entered by the user: name, phone, password, and email
+    Array.from(form.querySelectorAll('input')).map( input => {
+        // General requirements
+        if (input.value === '' || input.value.length < 3) {
+            input.style.border = "2px solid red"
+        }
+    })   
+
+    // Name and surname validations
+    let name = document.querySelector('.name');
+    let surname = document.querySelector('.surname');
+
+    // Applying regex patter to check if input value has numbers
+    if (/\d/.test(name.value)) {
+        name.parentElement.querySelector('.numeric-value').style.display = 'block';
+    }
+    if (/\d/.test(surname.value)) {
+        surname.parentElement.querySelector('.numeric-value').style.display = 'block';
+    }
+
+    // Phone number validation
+    let phone = document.querySelector('.phone');
+    // Cheking that all characters in string value are numbers
+    if (!/^\d+$/.test(phone.value)) {
+        errorPhone.style.display = 'block';
+    }
+
+    // Checking password has at least one letter, one number and length between 4 and 8
+    let password = document.querySelector(".password");
+    // Checking length of input and regex pattern for numbers and letters
+    if (password.value.length < 4 || password.value.length > 8 || 
+        !(/\d/.test(password.value) && /[a-zA-Z]/.test(password.value))) {
+        errorPassword.style.display = 'block';
+    }
+
+    // Email format validation
+    let email = document.querySelector(".email");
+    // Checking if has strcture xxx@xxx.xx
+    if (!/\S+@\S+\.\S+/.test(email.value)) {
+        errorEmail.style.display = 'block';
+    }
+
+    console.log(phone.value)
 }
--- js/grocery.js
+++ js/grocery.js
@@ -1,52 +1,10 @@
 // Exercise 11
 // Move this variable to a json file and load the data in this js
-var products = [
-    {
-        name: 'cooking oil',
-        price: 10.5,
-        type: 'grocery'
-    },
-    {
-        name: 'Pasta',
-        price: 6.25,
-        type: 'grocery'
-    },
-    {
-        name: 'Instant cupcake mixture',
-        price: 5,
-        type: 'grocery'
-    },
-    {
-        name: 'All-in-one',
-        price: 260,
-        type: 'beauty'
-    },
-    {
-        name: 'Zero Make-up Kit',
-        price: 20.5,
-        type: 'beauty'
-    },
-    {
-        name: 'Lip Tints',
-        price: 12.75,
-        type: 'beauty'
-    },
-    {
-        name: 'Lawn Dress',
-        price: 15,
-        type: 'clothes'
-    },
-    {
-        name: 'Lawn-Chiffon Combo',
-        price: 19.99,
-        type: 'clothes'
-    },
-    {
-        name: 'Toddler Frock',
-        price: 9.99,
-        type: 'clothes'
-    }
-]
+var products;
+fetch('../data/grocery.json')
+    .then( response => response.json())
+        .then( json => products = json);
+
 var cartList = [];
 var cart = [];
 var subtotal = {
@@ -73,40 +31,91 @@ function addToCartList(id) {
 
 // Exercise 2
 function cleanCart() {
-
+    cartList = [];
 }
 
 // Exercise 3
 function calculateSubtotals() {
     // 1. Create a for loop on the "cartList" array 
+    cartList.map( product => {
+        subtotal[product.type].value = subtotal[product.type].value + product.price;
+    })
     // 2. Implement inside the loop an if...else or switch...case to add the quantities of each type of product, obtaining the subtotals: subtotalGrocery, subtotalBeauty and subtotalClothes
+
 }
 
 // Exercise 4
 function calculateTotal() {
+    console.log(Object.values(subtotal))
     // Calculate total price of the cart either using the "cartList" array
+    // [David] Getting values (objects) from subtotals object. Then extracting value (type is not interesting at this point)
+    // and reducing value to sum all subtotals to get total amount.
+    total = Object.values(subtotal)
+        .map(subtotal => subtotal.value)
+            .reduce((previousValue, currentSubtotal) => {
+                return previousValue + currentSubtotal;
+                });
+    console.log(total)
 }
 
 // Exercise 5
 function applyPromotionsSubtotals() {
-
+    cartList.map( product => {})
 }
 
 // Exercise 6
-function generateCart() {
-    // Using the "cartlist" array that contains all the items in the shopping cart, 
-    // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
-}
+// [David] Removed because refactor
 
 // Exercise 7
 function applyPromotionsCart() {
     // Apply promotions to each item in the array "cart"
+    cart.map( (product, index) => {
+        if (product.name === 'cooking oil' && product.quantity > 2) {
+            cart[index].subtotalWithDiscount = cart[index].quantity * 10;
+        } else if (product.name === 'Instant cupcake mixture' && cart[index].quantity > 9) {
+            cart[index].subtotalWithDiscount = cart[index].quantity * (cart[index].price * 0.66);
+        }
+    })
 }
 
 // Exercise 8
 function addToCart(id) {
     // 1. Loop for to the array products to get the item to add to cart
+    // Note: no needed loop. Function is getting an index of the product in the array 
+    // so we can use it directly to select the desired product
+    const productSelected = products[id - 1];
     // 2. Add found product to the cartList array
+    // Refactor to apply logic of cart in each element added
+
+    let currentProductNames = cart.map( product => product.name );
+    if (currentProductNames.includes(productSelected.name)) {
+        // There are at least one product of the same type. We must find the index in the array and update it.
+        const productIndex = cart.findIndex( cartProduct => cartProduct.name === productSelected.name);
+        cart[productIndex] = {
+            ...cart[productIndex],
+            quantity: cart[productIndex].quantity + 1,
+            subtotal: cart[productIndex].subtotal + cart[productIndex].price,
+            subtotalWithDiscount: cart[productIndex].subtotal + cart[productIndex].price
+        }
+
+    } else { // Product is the first of it's type
+        cart.push({
+            ...productSelected,
+            quantity: 1,
+            subtotal: productSelected.price,
+            subtotalWithDiscount: productSelected.price
+        });
+    
+    }
+
+    // Updating subtotals with new product added
+    calculateSubtotals();
+
+    // Updating total amount
+    calculateTotal();
+
+    // Checking if promotions should be applied
+    applyPromotionsCart();
 }
 
 // Exercise 9
